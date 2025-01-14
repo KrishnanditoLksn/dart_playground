b219828876127d5c5afdcb6a53b631f09d33f7c2
Gorthdzx <krislaksana123@gmail.com>
2025-01-14 21:54:37 +0700

    init get setter

--- oop/animal.dart
+++ oop/animal.dart
@@ -1,18 +1,26 @@
 class Animal {
-  String name;
-  int age;
+  String _name = '';
+  int _age = 0;
+  double _weight = 0;
 
-  double weight;
+  Animal(this._name, this._age, this._weight);
 
-  Animal(this.name, this.age, this.weight);
+  set name(String value) {
+    _name = value;
+  }
+
+  double get weight => _weight;
+  int get age => _age;
+  String get name => _name;
+
 
   void eat() {
     print("$name is eating");
-    weight = weight + 0.2;
+    _weight = _weight + 0.2;
   }
 
   void sleep() {
-    print("$name is pooping");
+    print("$_name is pooping");
   }
 
   void poop() {
---
+++ oop/main.dart
@@ -0,0 +1,7 @@
+import 'animal.dart';
+
+void main() {
+  var mycat = Animal('Gray', 2, 3.4);
+  mycat.sleep();
+  print(mycat.name);
+}
